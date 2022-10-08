3f7b1f7a741596835e11d91d74adac1ed6998d2b
lvfd <lvfd@acca.com.cn>
2022-10-08 15:05:08 +0800

    bugfix

--- src/dovemgr/contentDocument.js
+++ src/dovemgr/contentDocument.js
@@ -1,13 +1,11 @@
 import {importCss, mgrcss} from './import'
 import {importJs, showModalDialog} from './import'
-import {show} from './functions'
+import {show, setInlineStyle} from './functions'
 
 export default function(iframe) {
   try {
 
     const href = iframe.contentWindow.location.href
-    console.log(href)
-
     const userweb = /dovepayUserWebMgrAction/
     if (userweb.test(href)) {
       console.log('用户系统')
@@ -24,11 +22,20 @@ export default function(iframe) {
       console.error('加载css错误')
     })
     css.addEventListener('load', () => {
+      inlineStyleHandler(iframe)
       show(iframe)
     })
-    
+
   } catch(e) {
     show(iframe)
     console.error('处理iframe.contentDocument失败', e.stack)
   }
+}
+
+function inlineStyleHandler(iframe) {
+  const expW = { width: '100%', boxSizing: 'border-box'}
+  const expWH = { width: '100%', height: '100%', boxSizing: 'border-box'}
+  setInlineStyle('div[style*="825px"]', expWH, {iframe: iframe})
+  setInlineStyle('div[style*="800px"]', expWH, {iframe: iframe})
+  setInlineStyle('div[style*="810px"]', expW, {iframe: iframe})
 }
--- src/dovemgr/functions.js
+++ src/dovemgr/functions.js
@@ -1,2 +1,23 @@
 export var show = (el) => el.classList.remove('uk-invisible')
 export var hide = (el) => el.classList.add('uk-invisible')
+
+export var setInlineStyle = (selector, styles, config) => {
+  try {
+    let root = document
+    if (config) {
+      if (config.iframe) {
+        root = config.iframe.contentDocument
+      }
+    }
+    const els = root.querySelectorAll(selector)
+    if (els.length < 1) return
+    if (typeof styles !== 'object') throw new Error('参数格式错误')
+    els.forEach((el) => {
+      for (let prop in styles) {
+        el.style[prop] = styles[prop]
+      }
+    })
+  } catch(e) {
+    console.error('设置行内style失败', e.stack)
+  }
+}
